e8d3024f55f05ed9d23d7a52b2cbabd6df97ffd9
AcbdsDev <gabaritandocoaching@gmail.com>
2024-07-08 13:48:06 -0300

    Add files via upload

---
+++ ListaLigada05.java
@@ -0,0 +1,19 @@
+package aula05;
+
+import aula03.Elemento;
+
+public class ListaLigada05<TIPO> {
+private Elemento primeiro;
+private Elemento ultimo;
+private int tamanho;
+
+public ListaLigada05(){
+this.tamanho =0;
+}
+public Elemento getPrimeiro(){
+    return primeiro;
+}
+public void setPrimeiro(Elemento primeiro){
+
+}
+}
